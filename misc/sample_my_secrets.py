4664be99a46eb6b60dfb162230ffa1fe9faadea0
Todd S <info@tascs.net>
2025-03-14 11:20:44 -0700

    moved

---
+++ misc/sample_my_secrets.py
@@ -0,0 +1,25 @@
+# HOST IP ADXs
+my_bluehost_ip = "x.x.x.x"  # your remote server's ip
+my_home_ip = "x.x.x.x"  # your home ip
+# SECURE ACCESS TO FILES ON ISP SERVER
+bluehost_user = "your_user"
+bluehost_password = "your_password"
+bluehost_key_path = r"path/to/your/key"
+# ACCESS TO LOCAL BLUEHOST-WEBLOGS DB
+local_dbuser = "your_dbuser"
+local_dbpassword = "your_dbpassword"
+local_dbhost = "your_dbhostname"
+local_dbname = "your_dbname"
+local_dburi = f"{local_dbuser}:{local_dbpassword}@{local_dbhost}/{local_dbname}"
+# PROD EMAIL INFO
+exchange_user = "user@user.tld"
+exchange_password = "password"
+exchange_mail_from = "user@user.tld"
+exchange_mailhost = 'mailserver.domain.tld'
+# RECIPIENT LIST
+email_to = ["user@user.tld", "user2@user.tld", "user3@user.tld"]
+# TEST EMAIL INFO
+postfix_user = "user@user.tld"
+postfix_password = "password"
+postfix_mail_from = "user@user.tld"
+postfix_mailhost = "server.domain.tld"
