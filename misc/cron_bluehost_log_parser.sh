340cdf4c816a4c8e1aff011d03f39c8af99986e2
Todd S <info@tascs.net>
2025-04-07 08:28:40 -0700

    rename

---
+++ misc/cron_bluehost_log_parser.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/todd/python_projects/bluehost_log_parser/src/bluehost_log_parser
+
+# source /home/todd/python_projects/rsyslog_processor/.venv/bin/activate
+
+/home/todd/.local/bin/uv run main.py
