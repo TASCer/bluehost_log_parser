d105d78e65d941edca554d9d9fe668b7334a6e09
Todd S <info@tascs.net>
2025-04-21 15:42:34 -0700

    pivot table issue, trying histogram

--- src/dashboard/components/bar_chart.py
+++ src/dashboard/components/bar_chart.py
@@ -27,7 +27,7 @@ def render(app: Dash, data: DataFrame) -> html.Div:
         if filtered_data.shape[0] == 0:
             return html.Div("No data selected.", id=ids.BAR_CHART)
 
-        # ISSSUE
+        #  ISSUE WITH KEYS month name
         def create_pivot_table() -> DataFrame:
             pt = filtered_data.pivot_table(
                 values=filtered_data["MONTH"],
