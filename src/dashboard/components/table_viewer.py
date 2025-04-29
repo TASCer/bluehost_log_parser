ba997b90bc0ecfbf2a10c2a7f0d98fa5c7a78e24
Todd S <info@tascs.net>
2025-04-29 10:30:01 -0700

    WIP w/ag grid

--- src/dashboard/components/table_viewer.py
+++ src/dashboard/components/table_viewer.py
@@ -1,13 +1,34 @@
-# https://dash.plotly.com/datatable
+# TODO GET TABLE VIEW INTERACTIVE w/dag
 from pandas import DataFrame
 from dash import dash_table, Dash, html
+from dash.dependencies import Input, Output
+from logging import Logger
+from . import ids
 
-import dash_ag_grid as dag
-
+import dash_ag_grid as dag 
 
 # deleteable=True only on column defining? Can remove rows!
-def render(app: Dash, source: DataFrame) -> html.Div:
-    data = source.to_dict("records")
-    filter_action = ["native"]
-    # df.sort_values("ACCESSED", ascending=False, inplace=True)
-    return dash_table.DataTable(data, editable=True, column_selectable="multi", cell_selectable=True, row_deletable=True, id="tbl")
+def render(app: Dash, data: DataFrame) -> html.Div:
+    df = data.copy()
+
+    # @app.callback(
+    #     Output(ids.PIE_CHART, "children"),
+    #     [
+    #         Input(ids.YEAR_DROPDOWN, "value"),
+    #         Input(ids.MONTH_DROPDOWN, "value"),
+    #         Input(ids.CODE_DROPDOWN, "value"),
+    #     ],
+    # )
+    # def update_pie_chart(
+    #     years: list[str], months: list[str], codes: list[str]
+    # ) -> html.Div:
+    # filtered_data = df.query(
+    #         "YEAR in @years and MONTH in @months and CODE in @codes"
+    #     )
+    if df.shape[0] == 0:
+        return html.Div("general.no_data", id=ids.PIE_CHART)
+    
+
+    return dash_table.DataTable(df.to_dict('records'),columns=[
+            {"name": i, "id": i, "deletable": True, "selectable": True, "editable":True} for i in df.columns])
+   
