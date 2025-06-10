db004099a90e7650dc3d4edca7a373171c4b868d
Todd S <info@tascs.net>
2025-06-10 09:34:34 -0700

    + ref_url dropdown

--- src/dashboard/components/pie_chart.py
+++ src/dashboard/components/pie_chart.py
@@ -1,4 +1,3 @@
-from turtle import textinput
 import plotly.express as px
 
 from dash import dcc, html, callback
@@ -16,13 +15,14 @@ def render(data: DataFrame) -> html.Div:
             Input(ids.YEAR_DROPDOWN, "value"),
             Input(ids.MONTH_DROPDOWN, "value"),
             Input(ids.CODE_DROPDOWN, "value"),
+            Input(ids.REFERRAL_DROPDOWN, "value"),
         ],
     )
     def update_pie_chart(
-        years: list[str], months: list[str], codes: list[str]
+        years: list[str], months: list[str], codes: list[str], referrals: list[str]
     ) -> html.Div:
         filtered_data: DataFrame = df.query(
-            "YEAR in @years and MONTH in @months and CODE in @codes"
+            "YEAR in @years and MONTH in @months and CODE in @codes and REF_URL in @referrals"
         )
         if filtered_data.shape[0] == 0:
             return html.Div("general.no_data", id=ids.PIE_CHART)
