8a9b9c498bfc9f465d5821f147ceb344032231fd
Todd S <info@tascs.net>
2025-06-15 13:39:09 -0700

    typed, cleaned

--- src/bluehost_log_parser/populate_tables.py
+++ src/bluehost_log_parser/populate_tables.py
@@ -1,6 +1,6 @@
 import logging
 
-from sqlalchemy import create_engine, exc, text
+from sqlalchemy import create_engine, exc, text, Engine
 from logging import Logger
 from bluehost_log_parser.db_checks import COUNTRIES_TABLE
 from bluehost_log_parser.my_secrets import local_dburi
@@ -9,26 +9,24 @@ from bluehost_log_parser.my_secrets import local_dburi
 logger: Logger = logging.getLogger(__name__)
 
 
-def countries():
+def countries() -> None:
     try:
-        engine = create_engine(f"mysql+pymysql://{local_dburi}")
+        engine: Engine = create_engine(f"mysql+pymysql://{local_dburi}")
 
     except (exc.SQLAlchemyError, exc.OperationalError) as e:
         logger.critical(str(e))
-        return False
 
     with open("../../misc/countries.txt") as fh:
-        data = fh.readlines()
+        data: list[str] = fh.readlines()
 
     with engine.connect() as conn, conn.begin():
         for c in data:
-            country = c.rstrip().split("\t")
+            country: list[str] = c.rstrip().split("\t")
             name, alpha2, alpha, number = country
 
             if "'" in name:
-                name = name.replace("'", "''")
+                name: str = name.replace("'", "''")
 
-            print(name, alpha, alpha2, number)
             conn.execute(
                 text(
                     f"""INSERT IGNORE into {COUNTRIES_TABLE} values('{name}', '{alpha}', '{alpha2}', '{number}');"""
