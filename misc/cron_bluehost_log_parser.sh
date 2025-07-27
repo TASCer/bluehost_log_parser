47cef4bd616c6658e08fab967b44bd1e8bc38dc1
TASCer <me@example.com>
2025-07-27 13:45:12 -0700

    using keychain now for crons

--- misc/cron_bluehost_log_parser.sh
+++ misc/cron_bluehost_log_parser.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-# TODO ISSUE WITH USING SSH AGENT KEYCHAIN?
-# NO WORKIE
-# SSH_AUTH_SOCK="/tmp/ssh-F3EIZbRPwwAu/agent.4956"
-# SSH_AGENT_PID=4957
-# SSH_AGENT_PID=`pgrep -U $USER ssh-agent`
-# for PID in $SSH_AGENT_PID; do
-#    let "FPID = $PID - 1"
-#    FILE=`find /tmp -path "*ssh*" -type s -iname "agent.$FPID"`
-#    export SSH_AGENT_PID="$PID" 
-#    export SSH_AUTH_SOCK="$FILE"
-# done
+# KEYCHAIN WIP
+# Load keychain variables and check for id_dsa
+[ -z "$HOSTNAME" ] && HOSTNAME=`uname -n`
+. $HOME/.keychain/$HOSTNAME-sh 2>/dev/null
+#ssh-add -l 2>/dev/null | grep -q id_dsa || exit 1
 
-# NO WORKIE
-# auth=`find /tmp -user $LOGNAME -type s -name "*agent*" -print 2>/dev/null`
-# SSH_AUTH_SOCK=$auth
-# export SSH_AUTH_SOCK
 
-cd /home/todd/python_projects/bluehost_log_parser/src/bluehost_log_parser
+# DEBIAN
+# cd /home/todd/python_projects/bluehost_log_parser/src/bluehost_log_parser
 
-# source /home/todd/python_projects/rsyslog_processor/.venv/bin/activate
+# RPI4
+cd /home/todd/bluehost_log_parser/src/bluehost_log_parser
 
 /home/todd/.local/bin/uv run main.py
+
+# https://linux.die.net/man/1/keychain
