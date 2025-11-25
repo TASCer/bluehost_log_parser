06c2756e8b9d64041360ede3fe24cc06476f959d
Todd S <info@tascs.net>
2025-11-25 15:28:24 -0700

    initial

---
+++ src/bluehost_log_parser/schema.py
@@ -0,0 +1,20 @@
+from typing import NamedTuple
+
+
+class LogEntry(NamedTuple):
+    """
+    Class represents a log entry from a webserver's logging system.
+
+    :param NamedTuple:
+    """
+
+    server_timestamp: str
+    SOURCE: str
+    CLIENT: str
+    AGENT: str
+    METHOD: str
+    FILE: str
+    RESPONSE: str
+    SIZE: str
+    REFERRER: str
+    HTTP: str
