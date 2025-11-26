a3dfab68b7d6198fcf93db5ec6aca5ec63faff2b
Todd S <info@tascs.net>
2025-11-26 12:01:09 -0700

    reworked sql commands, vars

--- src/bluehost_log_parser/create_views.py
+++ src/bluehost_log_parser/create_views.py
@@ -1,6 +1,7 @@
 import logging
 
 from bluehost_log_parser import my_secrets
+from bluehost_log_parser.insert_activity import SOHO_LOGS_TABLE, PUBLIC_LOGS_TABLE
 from logging import Logger
 from sqlalchemy import exc, create_engine, text
 
@@ -12,9 +13,7 @@ DB_USER: str = f"{my_secrets.local_dbuser}"
 DB_PW: str = f"{my_secrets.local_dbpassword}"
 DB_URI: str = f"{my_secrets.local_dburi}"
 
-LOGS_TABLE: str = "logs"
 SOURCES_TABLE: str = "sources"
-COUNTRIES_TABLE: str = "countries"
 VIEW_COUNTRY_ACTIVITY = "country_activity"
 
 
@@ -36,13 +35,13 @@ def all(engine) -> bool:
                     VIEW {VIEW_COUNTRY_ACTIVITY}
                     AS
                     SELECT
-                        COUNT(`bluehost-weblogs`.`logs`.`SOURCE`) AS `COUNT`,
-                        `bluehost-weblogs`.`sources`.`COUNTRY` AS `COUNTRY`
+                        COUNT(`bluehost-weblogs`.`{PUBLIC_LOGS_TABLE}`.`SOURCE`) AS `COUNT`,
+                        `bluehost-weblogs`.`{SOURCES_TABLE}`.`COUNTRY` AS `COUNTRY`
                     FROM
-                        (`bluehost-weblogs`.`logs`
-                        JOIN `bluehost-weblogs`.`sources` ON (`bluehost-weblogs`.`logs`.`SOURCE` = `bluehost-weblogs`.`sources`.`SOURCE`))
-                    GROUP BY `bluehost-weblogs`.`sources`.`COUNTRY`
-                    ORDER BY COUNT(`bluehost-weblogs`.`logs`.`SOURCE`) DESC;""")
+                        (`bluehost-weblogs`.`{PUBLIC_LOGS_TABLE}`
+                        JOIN `bluehost-weblogs`.`{SOURCES_TABLE}` ON (`bluehost-weblogs`.`{PUBLIC_LOGS_TABLE}`.`SOURCE` = `bluehost-weblogs`.`{SOURCES_TABLE}`.`SOURCE`))
+                    GROUP BY `bluehost-weblogs`.`{SOURCES_TABLE}`.`COUNTRY`
+                    ORDER BY COUNT(`bluehost-weblogs`.`{PUBLIC_LOGS_TABLE}`.`SOURCE`) DESC;""")
                 )
 
             return True
