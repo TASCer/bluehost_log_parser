60f63ba53d2f2f128ca538c8786bf513e96c66db
Todd S <info@tascs.net>
2025-11-27 12:26:02 -0700

    column rename, typing

--- src/dashboard/components/pie_chart.py
+++ src/dashboard/components/pie_chart.py
@@ -26,11 +26,11 @@ def render(data: DataFrame) -> html.Div:
         years: list[str], months: list[str], codes: list[str], referrals: list[str]
     ) -> html.Div:
         filtered_data: DataFrame = df.query(
-            "YEAR in @years and MONTH in @months and CODE in @codes and REF_URL in @referrals"
+            "YEAR in @years and MONTH in @months and CODE in @codes and REFERRER in @referrals"
         )
         if filtered_data.shape[0] == 0:
             return html.Div("general.no_data", id=ids.PIE_CHART)
-        fig = px.pie(filtered_data, values="CODE", names="REF_URL")
+        fig = px.pie(filtered_data, values="CODE", names="REFERRER")
         fig.update_traces(textinfo="label")
 
         logger.info("PIE CHART CREATED")
