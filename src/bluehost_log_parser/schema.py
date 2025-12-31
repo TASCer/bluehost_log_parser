932ad92064150bdda67a9fb459f4e9cc678fa63b
Todd S <info@tascs.net>
2025-12-31 10:36:18 -0700

    added: validators

--- src/bluehost_log_parser/schema.py
+++ src/bluehost_log_parser/schema.py
@@ -1,11 +1,12 @@
-from typing import NamedTuple
+from typing import Any
+from pydantic import BaseModel, field_validator, IPvAnyAddress
+from pydantic_extra_types.country import CountryAlpha2, CountryAlpha3
 
 
-class LogEntry(NamedTuple):
+class LogEntry(BaseModel):
     """
     Class represents a log entry from a webserver's logging system.
 
-    :param NamedTuple:
     """
 
     server_timestamp: str
@@ -19,3 +20,44 @@ class LogEntry(NamedTuple):
     REFERRER: str
     HTTP: str
     SITE: str
+
+    @field_validator("AGENT", "REFERRER")
+    def remove_btyes(cls, value: str) -> str:
+        if value.startswith("b'"):
+            value = value.replace("b'", "")
+            if value[-1] == "'":
+                value = value[:-2]
+        return value
+
+    @field_validator("CLIENT")
+    def remove_apostrophes(cls, value: str) -> str:
+        return value.replace("'", "")
+
+        return value
+
+
+class SourceEntry(BaseModel):
+    """
+    Class represents a log entry's source from a webserver's logging system.
+
+    """
+
+    SOURCE: str
+    COUNTRY: str
+    ALPHA2: CountryAlpha2
+    ALPHA3: CountryAlpha3
+    DESCRIPTION: str
+
+    # @field_validator('SOURCE', mode='before')
+    # @classmethod
+    # def extract_ip_from_string(cls, value: Any) -> str:
+    #     """
+    #     Validator to extract the IP address from the 'raw_input' field
+    #     using a regex capture group.
+    #     """
+    #     if isinstance(value, IPvAnyAddress):
+    #         return str(value)
+
+    #     # raw_string = info.data.get('raw_input')
+    #     else:
+    #         raise ValueError("raw_input field is missing")
