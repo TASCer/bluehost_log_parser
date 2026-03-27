44e666f5f9c125419e06deb00412145a353a7c61
Todd S <info@tascs.net>
2026-03-26 17:05:35 -0700

    .

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from pathlib import Path
 
-SAMPLE_LOG = Path(__file__).parent / "misc" / "sample_unzipped_logfile"
+SAMPLE_LOG = Path(__file__).parent / "assets" / "sample_unzipped_logfile"
 
 
 @pytest.fixture(scope="session")
@@ -10,6 +10,6 @@ def test_fetch_logs() -> list:
     print(SAMPLE_LOG.parent)
     print(SAMPLE_LOG.parent.parent.parent)
     with SAMPLE_LOG.open() as fh:
-        logs = fh.readlines()
+        logs: list[str] = fh.readlines()
     # print(logs)
     return logs
